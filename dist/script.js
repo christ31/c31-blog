bf10941f6502014f1425c6b0890c64b5cbf2fd05
christ31 <natanniel31@gmail.com>
2023-05-29 15:05:10 +0700

    first commit

---
+++ dist/script.js
@@ -0,0 +1,52 @@
+// It's show time
+// Thanks to Aaron Farrar https://codepen.io/afarrar/pen/JRaEjP
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 1 - 12
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  var session = "AM";
+  var light = "🌞";  
+
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October","November", "December"];
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+
+  if((h>5 && h<12) && session == "PM"){
+    light = "🌙";
+  } else if((h==12 || h<5) && session == "AM"){
+    light = "🌙";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + " " + session;
+
+  var year = date.getFullYear();
+  var day = date.getUTCDate();
+  var month = monthNames[date.getMonth()];
+
+  var fulldate = day + " " + month + " " + year;
+
+  // document.getElementById("MyClockDisplay").innerText = date+time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  
+  setTimeout(showTime, 50);
+  
+}
+
+showTime();
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkSecondary: '#c2410c',
+        'color-yt': '#F61C0D',
+        'color-osu!': '#e32373',
+        'color-steam': '#223246',
+        'color-discord': '#5865f2',
+        'color-github': '#171515',
+        'color-miku': '#14b8a6',
+        'color-wordpress': '#21759b',
+        'color-c31-primary': '#23252e',
+        'color-c31-secondary': '#4e5468',
+      }
+    },
+  },
+  plugins: [],
+}
+
